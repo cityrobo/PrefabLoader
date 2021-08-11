3632718bd8d2f1d54dbbdf9f368f5bea680418d2
Frityet <ambhogal01@gmail.com>
2021-08-11 10:34:01 -0700

    Added files

---
+++ Scripts/Editor/PrefabLoaderEditor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PrefabLoader))]
+public class PrefabLoaderEditor : Editor
+{
+    private PrefabLoader _prefabLoader;
+
+    private string _assetBundlePath;
+    private string _prefabToLoad;
+
+    private bool _bundleLoaded;
+    private bool _prefabsLoaded;
+
+    private string[] _prefabNames;
+    private int _selectedObjIndex;
+    private string _selectedObjName;
+
+    private string[] GetAssetNames(AssetBundle assetBundle)
+    {
+        EditorUtility.DisplayProgressBar("Loading Bundle", "Path: " + _assetBundlePath, 0f);
+        //Why is unity like this
+        string[] names = assetBundle.LoadAllAssets().ToDictionary(objName => objName.name).Keys.ToArray();
+        EditorUtility.DisplayProgressBar("Loading Bundle", "Path: " + _assetBundlePath, 1f);
+        EditorUtility.ClearProgressBar();
+        return names;
+    }
+    
+    private void OnEnable()
+    {
+        _prefabLoader = target as PrefabLoader;
+        _bundleLoaded = false;
+        _prefabsLoaded = false;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        if (GUILayout.Button("Select Bundle"))
+        {
+            _assetBundlePath = EditorUtility.OpenFilePanel("Select AssetBundle", String.Empty, String.Empty);
+            _bundleLoaded = true;
+        }
+
+        if (!_bundleLoaded) return;
+
+        EditorGUILayout.LabelField("Selected Bundle: " + _assetBundlePath);
+
+        
+        if (!_prefabsLoaded)
+        {
+            AssetBundle bundle = AssetBundle.LoadFromFile(_assetBundlePath);
+            _prefabNames = GetAssetNames(bundle);
+            bundle.Unload(false);
+            _prefabsLoaded = true;
+        }
+
+        if (_prefabNames != null)
+        {
+            _selectedObjIndex = EditorGUILayout.Popup(_selectedObjIndex, _prefabNames);
+            _prefabToLoad = _prefabNames[_selectedObjIndex];
+        }
+
+        if (_bundleLoaded && _prefabsLoaded)
+        {
+            _prefabLoader.assetBundlePath = _assetBundlePath;
+            _prefabLoader.prefabToLoad = _prefabToLoad;
+        }
+    }
+}
---
+++ Scripts/PrefabLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class PrefabLoader : MonoBehaviour
+{
+    public string assetBundlePath;
+    public string prefabToLoad;
+
+    private AssetBundle _bundle;
+
+    private void Awake()
+    {
+        if (String.IsNullOrEmpty(assetBundlePath) || !File.Exists(assetBundlePath))
+        {
+            EditorUtility.DisplayDialog("Error!", "Path to AssetBundle is empty, or the path in invalid", "☹");
+            return;
+        }
+        if (String.IsNullOrEmpty(prefabToLoad))
+        {
+            EditorUtility.DisplayDialog("Error!", "No prefab selected to load", "☹");
+            return;
+        }
+        
+        _bundle = AssetBundle.LoadFromFile(assetBundlePath);
+        Object obj = _bundle.LoadAsset(prefabToLoad);
+        Instantiate(obj, this.transform.position, this.transform.rotation);
+        _bundle.Unload(false);
+    }
+}
